f9dc71ade3a192d808c4c1244266d1b011440b0d
殷东昇 <youcancallmeallen@gmail.com>
2021-09-07 09:29:20 +0800

    csv

---
+++ README.md
@@ -0,0 +1,22 @@
+## run Instruction
+
+1. download codes
+![1](https://user-images.githubusercontent.com/38830527/132236474-84ebcaef-9311-4562-ba79-f9165cd38062.PNG)
+
+2. unzip package
+
+3. open folder cheerio_lianjia-main
+
+4. double click index.js to run
+
+        output json file `output.json`
+        output csv file `output.csv`
+
+## options
+
+run `npx json2csv -i output.json -o output.csv` convert json to csv format
+
+run `./index.js` to generate json file
+
+
+
---
+++ index.js
@@ -0,0 +1,73 @@
+const axios = require('axios')
+const fs = require('fs')
+var cheerio = require("cheerio")
+
+const url = 'https://bj.lianjia.com/xiaoqu/'
+const pageNumber = 40
+
+const xiaoqu = [
+    'dongcheng',
+    'xicheng',
+    'chaoyang',
+    'haidian',
+    'fengtai',
+    'shijingshan',
+    'tongzhou',
+    'changping',
+    'daxing',
+    'yizhuangkaifaqu',
+    'shunyi',
+    'fangshan',
+    'mentougou',
+    'pinggu',
+    'huairou',
+    'miyun',
+    'yanqing'
+]
+
+async function run() {
+    const request = []
+    for (let i = 0; i < xiaoqu.length; i++) {
+        for (let j = 0; j < pageNumber; j++) {
+            let _url = url + `${xiaoqu[i]}/pg${j + 1}cro21/`
+            await sleep(3000);
+            request.push(getData(_url))
+        }
+    }
+
+    Promise.all(request).then(res => {
+        console.log(res.flat(1).length);
+        writeFile(JSON.stringify(res.flat(1)))
+    })
+}
+run()
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function getData(url) {
+    const _result = []
+    const res = await axios.get(url)
+    var $ = cheerio.load(res.data)
+    let list = $('li.xiaoquListItem')
+    for (let i = 0; i < list.length; i++) {
+        var li = list.eq(i)
+        const title = query(li, '.title a')
+        const address = query(li, '.positionInfo')
+        const info = query(li, '.houseInfo')
+        const totalPrice = query(li, '.totalPrice')
+        const unitPrice = query(li, '.unitPrice')
+        const totalSellCount = query(li, '.totalSellCount')
+        _result.push({ title, address, info, totalPrice, totalSellCount, unitPrice })
+    }
+    return _result
+}
+
+function query(node, q) {
+    return node.find(q).text().trim()
+}
+
+function writeFile(data) {
+    fs.writeFileSync('./output1.json', data)
+}
